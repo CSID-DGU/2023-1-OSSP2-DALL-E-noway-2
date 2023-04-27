2051db52d335f99a1e5fc7ca97fafff60a775c38
sichoi42 <42.4.sichoi@gmail.com>
2023-04-27 13:09:54 +0900

    [FE] FEAT: vue 프로젝트 생성 및 App.vue 설정 #3

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
+import { onMounted } from 'vue';
+import HeaderBar from './components/common/HeaderBar.vue';
+import NavBar from './components/common/NavBar.vue';
+
+const setScreenSize = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
+
+onMounted(() => {
+  setScreenSize();
+});
+</script>
+
+<template>
+  <HeaderBar />
+  <RouterView />
+  <NavBar />
+</template>
+
+<style scoped></style>
---
+++ frontend/src/components/common/HeaderBar.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { RouterLink } from 'vue-router';
+</script>
+
+<template>
+  <nav class="wrap"></nav>
+</template>
+
+<style scoped></style>
