8093d165764342b0121b867fcadfc6529cf0027f
sichoi42 <42.4.sichoi@gmail.com>
2023-05-01 22:01:19 +0900

    Config 설정 및 Swagger 설정 추가 #5

---
+++ backend/src/config/configuration.ts
@@ -0,0 +1,26 @@
+export default () => ({
+  port: parseInt(process.env.PORT, 10),
+  jwt: {
+    secret: process.env.JWT_SECRET,
+    expiresIn: process.env.JWT_EXPIRESIN,
+  },
+  kakao: {
+    clientID: process.env.KAKAO_CLIENT_ID,
+    clientSecret: process.env.KAKAO_CLIENT_SECRET,
+    adminKey: process.env.KAKAO_ADMIN_KEY,
+    callbackURL: process.env.KAKAO_CALLBACK_URL,
+  },
+  naver: {
+    clientID: process.env.NAVER_CLIENT_ID,
+    clientSecret: process.env.NAVER_CLIENT_SECRET,
+    callbackURL: process.env.NAVER_CALLBACK_URL,
+  },
+  google: {
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  },
+  debug: {
+    log: process.env.DEBUG_LOG === 'true' ? true : false,
+  },
+});
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Dream Vision API')
+    .setDescription('Dream Vision API 명세서')
+    .setVersion('1.0')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get<number>('port');
+  await app.listen(port);
 }
 bootstrap();
