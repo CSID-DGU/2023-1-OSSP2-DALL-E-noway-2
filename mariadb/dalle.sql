9caf594649e0217b768573c0b9a50554c62c12ee
sichoi42 <42.4.sichoi@gmail.com>
2023-05-07 20:47:49 +0900

    [BE] DDL 업데이트(작성중)

--- mariadb/dalle.sql
+++ mariadb/dalle.sql
@@ -16,25 +16,228 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `test`
+-- Table structure for table `category`
 --
 
-DROP TABLE IF EXISTS `test`;
+DROP TABLE IF EXISTS `category`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `test` (
-  `test_id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`test_id`)
+CREATE TABLE `category` (
+  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`category_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `test`
+-- Dumping data for table `category`
 --
 
-LOCK TABLES `test` WRITE;
-/*!40000 ALTER TABLE `test` DISABLE KEYS */;
-/*!40000 ALTER TABLE `test` ENABLE KEYS */;
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `diary_category`
+--
+
+DROP TABLE IF EXISTS `diary_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `diary_category` (
+  `diary_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  PRIMARY KEY (`diary_id`,`category_id`),
+  KEY `diary_category_fk` (`category_id`),
+  CONSTRAINT `category_dream_diary_fk` FOREIGN KEY (`diary_id`) REFERENCES `dream_diary` (`diary_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `diary_category_fk` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `diary_category`
+--
+
+LOCK TABLES `diary_category` WRITE;
+/*!40000 ALTER TABLE `diary_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `diary_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `diary_stat`
+--
+
+DROP TABLE IF EXISTS `diary_stat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `diary_stat` (
+  `diary_stat_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `year` int(11) NOT NULL,
+  `month` int(11) NOT NULL,
+  `average_score` float DEFAULT NULL,
+  PRIMARY KEY (`diary_stat_id`),
+  KEY `diary_stat_user_null_fk` (`user_id`),
+  CONSTRAINT `diary_stat_user_null_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `diary_stat`
+--
+
+LOCK TABLES `diary_stat` WRITE;
+/*!40000 ALTER TABLE `diary_stat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `diary_stat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `diary_stat_category`
+--
+
+DROP TABLE IF EXISTS `diary_stat_category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `diary_stat_category` (
+  `diary_stat_id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `average_score` float DEFAULT NULL,
+  PRIMARY KEY (`category_id`,`diary_stat_id`),
+  KEY `diary_stat_category_fk` (`diary_stat_id`),
+  CONSTRAINT `category_diary_stat_fk` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `diary_stat_category_fk` FOREIGN KEY (`diary_stat_id`) REFERENCES `diary_stat` (`diary_stat_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `diary_stat_category`
+--
+
+LOCK TABLES `diary_stat_category` WRITE;
+/*!40000 ALTER TABLE `diary_stat_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `diary_stat_category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `dream_diary`
+--
+
+DROP TABLE IF EXISTS `dream_diary`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dream_diary` (
+  `diary_id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `dream_score` tinyint(4) NOT NULL,
+  `view_count` int(11) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `image_url` varchar(255) NOT NULL,
+  `disclosure_scope` varchar(255) NOT NULL DEFAULT 'PRIVATE',
+  `interpretation` text DEFAULT NULL,
+  PRIMARY KEY (`diary_id`),
+  KEY `dream_diary_user_fk` (`user_id`),
+  CONSTRAINT `dream_diary_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `dream_score_check` CHECK (`dream_score` in (1,2,3,4,5)),
+  CONSTRAINT `check_disclosure_scope` CHECK (`disclosure_scope` in ('PRIVATE','PUBLIC','LIMITED_PRIVATE'))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `dream_diary`
+--
+
+LOCK TABLES `dream_diary` WRITE;
+/*!40000 ALTER TABLE `dream_diary` DISABLE KEYS */;
+/*!40000 ALTER TABLE `dream_diary` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `follow`
+--
+
+DROP TABLE IF EXISTS `follow`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `follow` (
+  `follower_id` int(11) NOT NULL,
+  `following_id` int(11) NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`follower_id`),
+  KEY `following_user_fk` (`following_id`),
+  CONSTRAINT `follower_user_fk` FOREIGN KEY (`follower_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `following_user_fk` FOREIGN KEY (`following_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `follow`
+--
+
+LOCK TABLES `follow` WRITE;
+/*!40000 ALTER TABLE `follow` DISABLE KEYS */;
+/*!40000 ALTER TABLE `follow` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `oauth`
+--
+
+DROP TABLE IF EXISTS `oauth`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `oauth` (
+  `user_id` int(11) NOT NULL,
+  `oauth_id` varchar(255) NOT NULL,
+  `access_token` varchar(255) DEFAULT NULL,
+  `provider` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `oauth_user_fk` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `check_provider` CHECK (`provider` in ('NAVER','KAKAO','GOOGLE'))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `oauth`
+--
+
+LOCK TABLES `oauth` WRITE;
+/*!40000 ALTER TABLE `oauth` DISABLE KEYS */;
+/*!40000 ALTER TABLE `oauth` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `nickname` varchar(255) NOT NULL,
+  `image_url` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `presentation` varchar(255) DEFAULT NULL,
+  `credits` int(11) NOT NULL DEFAULT 3,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `nickname` (`nickname`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (3,'test1','test1','test1','test1',NULL,3);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -46,4 +249,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-05-03  0:04:14
+-- Dump completed on 2023-05-07 20:24:25
