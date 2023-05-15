0b87ce9e80e55626540f764d8b9086e27b18727b
nicolao00 <nicolao00@naver.com>
2023-05-16 00:44:26 +0900

    [BE] FEAT: 답글 생성 기능 구현 #17

--- backend/src/comment/comment.controller.ts
+++ backend/src/comment/comment.controller.ts
@@ -4,31 +4,82 @@ import { Comment } from 'src/entities/comment.entity';
 import { User } from 'src/entities/user.entity';
 import { FilterType } from 'src/enum/filter.type';
 import { CommentService } from './comment.service';
-import { CommentResponseDto } from './dto/comment.response.dto';
-
+import { CommentResponseDto } from '../dto/comment.response.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 // comment DB에 내용이 담기는 colum이 없어서 content 필드명으로 테이블에 추가해서 내용 저장해야함!
-@Controller('/api/comments')
+@ApiTags('Comment')
+@Controller('comments')
 export class CommentController {
-    constructor(private commentService: CommentService) {}
+  constructor(private readonly commentService: CommentService) {}
 
-    // 조회 기능 / 글의 종류, 글 id를 받아와 해당하는 글의 댓글들을 모두 가져오는 API
-    @Get('/:filter_type/:id')
-    async getAllComments(@Param('filter_type') filterType: FilterType, @Param('id') id: number): Promise<Comment[]> {
-        return await this.commentService.getAllComments(filterType, id);
-    }
+  @ApiOperation({
+    summary: '댓글 조회',
+    description:
+      '글의 종류, 글 id를 받아와 해당하는 글의 댓글들을 모두 가져옵니다.',
+  })
+  @Get('/:filter_type/:id')
+  async getAllComments(
+    @Param('filter_type') filterType: FilterType,
+    @Param('id') id: number,
+  ): Promise<Comment[]> {
+    return await this.commentService.getAllComments(filterType, id);
+  }
 
-    // 댓글 생성 기능 / 글의 종류, 글 id, 글 내용을 받아와 해당하는 글에 댓글을 등록하는 API
-    @Post('/:filter_type/:id')
-    async createComment(@Body() commentResponseDto:CommentResponseDto,
+  @ApiOperation({
+    summary: '댓글 생성',
+    description:
+      '글의 종류, 글 id, 글 내용을 받아와 해당하는 글에 댓글을 등록합니다.',
+  })
+  @Post('/:filter_type/:id')
+  async createComment(
+    @Body() commentResponseDto: CommentResponseDto,
     @Param('filter_type') filter_type: FilterType,
-     @Param('id') id: number,
-     @GetUser() user: User,
-     ){
-        commentResponseDto.filterType = filter_type;
-        commentResponseDto.id = id;
+    @Param('id') id: number,
+    @GetUser() user: User,
+  ) {
+    commentResponseDto.filterType = filter_type;
+    commentResponseDto.id = id;
 
-        const result = await this.commentService.createComment(commentResponseDto, user);
-        return result.commentId; 
-    }
+    const result = await this.commentService.createComment(
+      commentResponseDto,
+      user,
+    );
+    return result.commentId;
+  }
+
+  @ApiOperation({
+    summary: '답글 생성',
+    description: '글의 종류, 글 id, 부모 댓글 id를 받아와 답글을 등록합니다.',
+  })
+  @Post('/:filter_type/:id/:comment_id')
+  async createReply(
+    @Body() commentResponseDto: CommentResponseDto,
+    @Param('filter_type') filter_type: FilterType,
+    @Param('id') id: number,
+    @Param('comment_id') comment_id: number,
+    @GetUser() user: User,
+  ) {
+    commentResponseDto.filterType = filter_type;
+    commentResponseDto.id = id;
+    commentResponseDto.parentCommentId = comment_id;
+    const result = await this.commentService.createReply(
+      commentResponseDto,
+      user,
+    );
+    return {
+      comment_id: result.commentId,
+      parent_comment_id: result.parentCommentId,
+    };
+  }
+
+  @ApiOperation({
+    summary: '댓글 삭제',
+    description:
+      '글의 종류, 글 id, 댓글 id를 받아와 해당하는 댓글을 삭제합니다.',
+  })
+  @Delete('/:filter_type/:id/:comment_id')
+  async deleteComment(@Param('comment_id') commentId: number) {
+    return await this.commentService.deleteComment(commentId);
+  }
 }
--- backend/src/comment/comment.service.ts
+++ backend/src/comment/comment.service.ts
@@ -4,40 +4,75 @@ import { Comment } from 'src/entities/comment.entity';
 import { User } from 'src/entities/user.entity';
 import { FilterType } from 'src/enum/filter.type';
 import { Repository } from 'typeorm';
-import { CommentResponseDto } from './dto/comment.response.dto';
+import { CommentResponseDto } from '../dto/comment.response.dto';
 
 @Injectable()
 export class CommentService {
-    constructor(
-        @InjectRepository(Comment)
-        private commentRepository: Repository<Comment>,
-    ) {}
+  constructor(
+    @InjectRepository(Comment)
+    private commentRepository: Repository<Comment>,
+  ) {}
 
-    // 조회 기능 / 글의 종류와 글 id를 받아와 해당하는 글의 댓글들을 모두 가져오는 API
-    async getAllComments(filterType: FilterType, id: number): Promise <Comment[]> {
-        const query = this.commentRepository.createQueryBuilder('comment')
-        .where('comment.filterType = :filterType', {filterType})
-        .andWhere('comment.id = :id', {id});
+  // 조회 기능 / 글의 종류와 글 id를 받아와 해당하는 글의 댓글들을 모두 가져오는 API
+  async getAllComments(filterType: FilterType, id: number): Promise<Comment[]> {
+    const query = this.commentRepository
+      .createQueryBuilder('comment')
+      .where('comment.filterType = :filterType', { filterType })
+      .andWhere('comment.id = :id', { id });
 
-        const comments = await query.getMany();
-        return comments;
+    const comments = await query.getMany();
+    return comments;
+  }
+
+  // 댓글 생성 기능 / 글의 종류, 글 id, 글 내용을 받아와 해당하는 글에 댓글을 등록하는 API
+  async createComment(
+    commentResponseDto: CommentResponseDto,
+    user: User,
+  ): Promise<Comment> {
+    // const comment = new Comment()
+    // comment.id = commentResponseDto.id;
+    // comment.content = commentResponseDto.content;
+    // comment.filterType = commentResponseDto.filterType;
+    // comment.parentCommentId = null;
+    // comment.createdAt = new Date();
+    // comment.userId = user.userId;
+    // return await this.commentRepository.save(comment); //아래와 같이 간략화할 수 있지만 확실하진 않아서 주석처리
+
+    const comment = this.commentRepository.create(commentResponseDto);
+    comment.parentCommentId = null;
+    comment.createdAt = new Date();
+    comment.userId = user.userId;
+    return await this.commentRepository.save(comment);
+  }
+
+  // 답글 생성 기능 / 글의 종류, 글 id, 부모 댓글 id를 받아와 답글을 등록하는 API
+  async createReply(
+    commentResponseDto: CommentResponseDto,
+    user: User,
+  ): Promise<Comment> {
+    // const replyComment = new Comment();
+    // replyComment.id = commentResponseDto.id;
+    // replyComment.content = commentResponseDto.content;
+    // replyComment.filterType = commentResponseDto.filterType;
+    // replyComment.parentCommentId = commentResponseDto.parentCommentId;
+    // replyComment.createdAt = new Date();
+    // replyComment.userId = user.userId;
+    // return await this.commentRepository.save(replyComment);//아래와 같이 간략화할 수 있지만 확실하진 않아서 주석처리
+
+    const replyComment = this.commentRepository.create(commentResponseDto);
+    replyComment.createdAt = new Date();
+    replyComment.userId = user.userId;
+
+    return await this.commentRepository.save(replyComment);
+  }
+
+  // 답글 삭제 기능 / 글의 종류와 글 id를 받아와 해당하는 댓글을 삭제하는 API
+  async deleteComment(commentId: number) {
+    const result = await this.commentRepository.delete(commentId);
+
+    if (result.affected === 0) {
+      throw new NotFoundException(`Can't find CommentId with id ${commentId}`);
     }
-
-    // 댓글 생성 기능 / 글의 종류, 글 id, 글 내용을 받아와 해당하는 글에 댓글을 등록하는 API
-    async createComment(commentResponseDto:CommentResponseDto, user: User): Promise <Comment> {
-        // const comment = new Comment()
-        // comment.id = commentResponseDto.id;
-        // comment.content = commentResponseDto.content;
-        // comment.filterType = commentResponseDto.filterType;
-        // comment.parentCommentId = null;
-        // comment.createdAt = new Date();
-        // comment.userId = user.userId;
-        // return await this.commentRepository.save(comment); //아래와 같이 간략화할 수 있지만 확실하진 않아서 주석처리
-
-        const comment = this.commentRepository.create(commentResponseDto);
-        comment.parentCommentId = null;
-        comment.createdAt = new Date();
-        comment.userId = user.userId;
-        return await this.commentRepository.save(comment);
-    }
+    //console.log('result', result);
+  }
 }
