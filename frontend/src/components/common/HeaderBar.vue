0dea309c7bdb05543adcb08089f9d9d5227a642b
sichoi42 <42.4.sichoi@gmail.com>
2023-05-19 20:51:49 +0900

    [FE] FEAT: 헤더바, 네비게이션바 스타일 적용 #24

--- frontend/src/components/common/HeaderBar.vue
+++ frontend/src/components/common/HeaderBar.vue
@@ -4,11 +4,23 @@ import { RouterLink } from 'vue-router';
 </script>
 
 <template>
-  <nav class="wrap">
-    <nav>
-      <RouterLink to="/"> LOGIN </RouterLink>
-    </nav>
-  </nav>
+  <header class="wrap">
+    <div class="container">
+      <h1 class="logo">DREAM VISION</h1>
+    </div>
+  </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+.wrap {
+  @apply bg-black text-white py-4;
+}
+
+.container {
+  @apply mx-auto flex items-center justify-center px-4;
+}
+
+.logo {
+  @apply text-xl font-bold;
+}
+</style>
--- frontend/src/components/common/NavBar.vue
+++ frontend/src/components/common/NavBar.vue
@@ -1,16 +1,65 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import IconHome from '@/components/icons/IconHome.vue';
+import IconGlobal from '@/components/icons/IconGlobal.vue';
+import IconCalendar from '@/components/icons/IconCalendar.vue';
+import IconProfile from '@/components/icons/IconProfile.vue';
 </script>
 
 <template>
   <div class="wrap">
     <nav>
-      <RouterLink to="/"> HOME </RouterLink>
-      <RouterLink to="/board-list"> BOARD </RouterLink>
-      <RouterLink to="/calendar"> CALENDAR </RouterLink>
-      <RouterLink to="/profile"> PROFILE </RouterLink>
+      <RouterLink to="/home">
+        <IconHome :opacity="$route.name === 'dream-diary-feed' ? 1 : 0.5" />
+      </RouterLink>
+      <RouterLink to="/board-list">
+        <IconGlobal :opacity="$route.name === 'board-list' ? 1 : 0.5" />
+      </RouterLink>
+      <RouterLink to="/calendar">
+        <IconCalendar :opacity="$route.name === 'calendar' ? 1 : 0.5" />
+      </RouterLink>
+      <RouterLink to="/profile">
+        <IconProfile :opacity="$route.name === 'profile' ? 1 : 0.5" />
+      </RouterLink>
     </nav>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.wrap {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 50px;
+  background-color: black;
+  filter: var(--menu-shadow);
+  z-index: 9;
+  padding: 0 30px;
+  user-select: none;
+}
+
+@media (min-width: 425px) {
+  .wrap {
+    max-width: 425px;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+}
+
+nav {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 100%;
+}
+nav a {
+  display: block;
+  height: 100%;
+  width: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+</style>
