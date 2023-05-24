acb081e5f12f319d5d689b4e21733aaab4ad6654
sichoi42 <42.4.sichoi@gmail.com>
2023-05-25 01:02:46 +0900

    [FE] FEAT: 헤더바에 뒤로가기 버튼 조건부 추가 #31

---
+++ frontend/src/views/FollowView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Follow</h1>
+  </main>
+</template>
+
+<style scoped></style>
---
+++ frontend/src/views/GenerateImageView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <h1>Generate Image</h1>
+  </main>
+</template>
+
+<style scoped></style>
