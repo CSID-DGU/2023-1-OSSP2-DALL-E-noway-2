bc5f46e170503d9ba254da34a7eef9e25ebd8b70
sichoi42 <42.4.sichoi@gmail.com>
2023-05-26 14:35:05 +0900

    [BE] FEAT: 유저 크레딧 정보 API 추가 #27

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -4,9 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { OAuth } from 'src/entities/oauth.entity';
 import { ImageRequests } from 'src/entities/image.requests.entity';
+import { UserController } from './user.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User, OAuth, ImageRequests])],
+  controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
 })
