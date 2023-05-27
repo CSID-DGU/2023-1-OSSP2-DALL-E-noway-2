a76a9cfd6c8df70c4ccc5df6c226ce92f5ebc3de
nicolao00 <nicolao00@naver.com>
2023-05-27 15:04:04 +0900

    [BE] FEAT: 기타 게시글 좋아요 기능 구현 #21

--- backend/src/board/board.controller.ts
+++ backend/src/board/board.controller.ts
@@ -1,10 +1,22 @@
-import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { GetUser } from 'src/decorator/user.decorator';
+import { PostLikeDto } from 'src/dto/post.like.dto';
 import { PostRequestDto } from 'src/dto/post.request.dto';
 import { UserDto } from 'src/dto/user.dto';
+import { Favorite } from 'src/entities/favorite.entity';
 import { BoardType } from 'src/enum/board.type';
+import { FilterType } from 'src/enum/filter.type';
 import { BoardService } from './board.service';
 
 @ApiTags('Board')
@@ -28,5 +40,60 @@ export class BoardController {
     postRequestDto.userId = user.userId;
     const result = await this.boardService.createPost(postRequestDto);
     return result.postId;
+  }
+
+  // 입력으로 받아온 boardType 자료형과 Favorite Entity의 filterType 자료형이 일치하지 않기에 자료형을 매칭시켜줄 함수.
+  boardTypeMatch(postLikeDto: PostLikeDto, boardType: BoardType) {
+    switch (boardType) {
+      case BoardType.FREE:
+        postLikeDto.filterType = FilterType.FREE;
+        break;
+      case BoardType.TIP:
+        postLikeDto.filterType = FilterType.TIP;
+        break;
+      case BoardType.REQUEST:
+        postLikeDto.filterType = FilterType.REQUEST;
+        break;
+      default:
+        throw new HttpException('Invalid board type', HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @ApiOperation({
+    summary: '게시글 좋아요 설정',
+    description:
+      'post_id, post_type, userId 정보를 바탕으로 해당하는 게시글 좋아요를 설정합니다.',
+  })
+  @Post('/posts/:post_id/:post_type/like')
+  @UseGuards(AuthGuard('jwt'))
+  async postLike(
+    @Param('post_id') postId: number,
+    @Param('post_type') boardType: BoardType,
+    @GetUser() user: UserDto,
+  ): Promise<Favorite> {
+    const postLikeDto = new PostLikeDto();
+    postLikeDto.id = postId;
+    this.boardTypeMatch(postLikeDto, boardType);
+    postLikeDto.userId = user.userId;
+
+    return await this.boardService.postLike(postLikeDto);
+  }
+
+  @ApiOperation({
+    summary: '게시글 좋아요 취소',
+    description: 'post_id에 해당하는 게시글 좋아요를 취소합니다.',
+  })
+  @Delete('/posts/:post_id/:post_type/like')
+  @UseGuards(AuthGuard('jwt'))
+  async postLikeCancel(
+    @Param('post_id') postId: number,
+    @Param('post_type') boardType: BoardType,
+    @GetUser() user: UserDto,
+  ) {
+    const postLikeDto = new PostLikeDto();
+    postLikeDto.id = postId;
+    this.boardTypeMatch(postLikeDto, boardType);
+    postLikeDto.userId = user.userId;
+    return await this.boardService.postLikeCancel(postLikeDto);
   }
 }
--- backend/src/board/board.service.ts
+++ backend/src/board/board.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { PostLikeDto } from 'src/dto/post.like.dto';
 import { PostRequestDto } from 'src/dto/post.request.dto';
-import { UserDto } from 'src/dto/user.dto';
 import { Board } from 'src/entities/board.entity';
 import { Bookmark } from 'src/entities/bookmark.entity';
 import { Favorite } from 'src/entities/favorite.entity';
@@ -12,6 +12,12 @@ export class BoardService {
   constructor(
     @InjectRepository(Board)
     private boardRepository: Repository<Board>,
+
+    @InjectRepository(Favorite)
+    private favoriteRepository: Repository<Favorite>,
+
+    @InjectRepository(Bookmark)
+    private bookmarkRepository: Repository<Bookmark>,
   ) {}
 
   // 게시글 생성 기능 / 게시글의 종류와 게시글 내용을 받아와 해당하는 게시판에 게시글을 등록하는 API
@@ -19,5 +25,17 @@ export class BoardService {
     const board = this.boardRepository.create(postRequestDto);
     board.createdAt = new Date();
     return await this.boardRepository.save(board);
+  }
+
+  // 게시글 좋아요 설정 기능 / 게시글의 ID를 받아와 해당하는 게시글의 좋아요를 설정하는 API
+  async postLike(postLikeDto: PostLikeDto): Promise<Favorite> {
+    const board = await this.favoriteRepository.create(postLikeDto);
+    board.createdAt = new Date();
+    return await this.favoriteRepository.save(board);
+  }
+
+  // 게시글 좋아요 취소 기능 // post_id, post_type, userId를 바탕으로 해당 게시글의 좋아요를 취소합니다.
+  async postLikeCancel(postLikeDto: PostLikeDto) {
+    return await this.favoriteRepository.delete(postLikeDto);
   }
 }
