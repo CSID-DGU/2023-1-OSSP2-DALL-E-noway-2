190b72044044714058160727cc00115572176407
sssungjin <dkdlwls576@gmail.com>
2023-05-30 11:47:38 +0900

    [BE] FIX: 이미지 업로드 경로 수정 #39

--- backend/src/profile/profile.controller.ts
+++ backend/src/profile/profile.controller.ts
@@ -98,39 +98,6 @@ export class ProfileController {
     }
   }
 
-  // @ApiOperation({
-  //   summary: '유저 프로필 수정',
-  //   description:
-  //     '유저의 프로필 정보(프로필 사진,닉네임,자기소개)를 수정합니다.',
-  // })
-  // @Put(':userId/profile')
-  // @UseGuards(AuthGuard('jwt'))
-  // async updateProfile(
-  //   @Body() profileUpdateDto: ProfileUpdateRequestDto,
-  //   @GetUser() user: UserDto,
-  // ): Promise<ProfileUpdateRequestDto> {
-  //   try {
-  //     const profile = await this.profileService.updateProfile(
-  //       profileUpdateDto,
-  //       user.userId,
-  //     );
-
-  //     return profile;
-  //   } catch (err) {
-  //     //요청 파라미터가 잘못된 경우
-  //     if (err instanceof TypeError || err instanceof Error) {
-  //       throw new BadRequestException(err.message);
-  //     }
-  //     //존재하지 않는 유저(탈퇴한 유저)
-  //     if (err instanceof ForbiddenException) {
-  //       throw new ForbiddenException(err.message);
-  //     }
-
-  //     //기타 에러 전체에서 처리
-  //     throw new InternalServerErrorException(err.message);
-  //   }
-  // }
-
   @ApiOperation({
     summary: '유저 프로필 수정',
     description:
@@ -167,8 +134,11 @@ export class ProfileController {
     try {
       let imageUrl: string;
 
+      // if (image) {
+      //   imageUrl = `${this.configService.get<string>('beHost')}/${image.path}`;
+      // }
       if (image) {
-        imageUrl = `${this.configService.get<string>('beHost')}/${image.path}`;
+        imageUrl = `${image.path}`;
       }
 
       const profile = await this.profileService.updateProfile(
