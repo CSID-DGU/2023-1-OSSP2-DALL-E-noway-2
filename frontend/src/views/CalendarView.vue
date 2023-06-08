6a154998ad3d6da4e96d2315b6c93fc975a92777
sichoi42 <42.4.sichoi@gmail.com>
2023-06-09 02:25:33 +0900

    [FE] FEAT: 달력 뷰에 스와이퍼와 CharJS 적용 및 통계 API 연동 #59

--- frontend/src/views/CalendarView.vue
+++ frontend/src/views/CalendarView.vue
@@ -5,7 +5,12 @@ import LoadingAnimation from '@/components/common/LoadingAnimation.vue';
 import { useCalendarInfoStore } from '@/stores/calendar.info.store';
 import { useMyInfoStore } from '@/stores/my.info.store';
 import { getStorage } from '@/utils/local.storage';
-import { onMounted, ref, watch, type Ref, computed } from 'vue';
+import { onMounted, ref, watch } from 'vue';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper';
+import CountDougnutStat from '@/components/calendar/CountDoughnutStat.vue';
 
 const { getUser, apiGetUser } = useMyInfoStore();
 const calendarInfo = useCalendarInfoStore();
@@ -68,49 +73,63 @@ watch(showCalendarList, (value) => {
 watch(showSelectedDiaryFeed, (value) => {
   selectedDiaryFeed.value = value;
 });
+
+const modules = [Pagination];
 </script>
 
 <template>
   <main>
-    <CalendarPagination />
-    <div class="calendar">
-      <CalendarWeek />
-      <div v-if="isLoading" class="loading">
-        <LoadingAnimation />
-      </div>
-      <div v-else id="days" class="days">
-        <div v-for="i in day" :key="i" class="noday"></div>
-        <div
-          v-for="day in calendarList.days"
-          :key="day.day"
-          @click="clickDate(day.day)"
-          class="day"
-          :style="{
-            background: getDateBgColor(day.dreamScore),
-            color: 'white',
-          }"
-          :class="{
-            today: checkToday(day.day),
-            selected: day.day === showSelectedDate(),
-          }"
-        >
-          {{ day.day }}
+    <swiper :pagination="true" :modules="modules" class="mySwiper">
+      <swiper-slide>
+        <div>
+          <CalendarPagination />
+          <div class="calendar">
+            <CalendarWeek />
+            <div v-if="isLoading" class="loading">
+              <LoadingAnimation />
+            </div>
+            <div v-else id="days" class="days">
+              <div v-for="i in day" :key="i" class="noday"></div>
+              <div
+                v-for="day in calendarList.days"
+                :key="day.day"
+                @click="clickDate(day.day)"
+                class="day"
+                :style="{
+                  background: getDateBgColor(day.dreamScore),
+                  color: 'white',
+                }"
+                :class="{
+                  today: checkToday(day.day),
+                  selected: day.day === showSelectedDate(),
+                }"
+              >
+                {{ day.day }}
+              </div>
+            </div>
+          </div>
+          <div v-if="selectedDiaryFeed && selectedDiaryFeed.diaryId !== 0">
+            <RouterLink :to="`/dream-diary/${selectedDiaryFeed.diaryId}`">
+              <div class="feed-container">
+                <h2 class="feed-title">{{ selectedDiaryFeed.title }}</h2>
+                <p class="feed-user">{{ selectedDiaryFeed.nickname }}</p>
+                <p class="feed-content">
+                  {{ selectedDiaryFeed.content }}
+                </p>
+                <p class="feed-view">👀 {{ selectedDiaryFeed.viewCount }}</p>
+                <img :src="selectedDiaryFeed.imageUrl" alt="Post Image" />
+              </div>
+            </RouterLink>
+          </div>
+          <div v-else class="feed-container">
+            <p class="no-diary-message">이 날에는 일기를 쓰지 않았어요</p>
+          </div>
         </div>
-      </div>
-    </div>
-    <div v-if="selectedDiaryFeed && selectedDiaryFeed.diaryId !== 0">
-      <RouterLink :to="`/dream-diary/${selectedDiaryFeed.diaryId}`">
-        <div class="feed-container">
-          <h2 class="feed-title">{{ selectedDiaryFeed.title }}</h2>
-          <p class="feed-user">{{ selectedDiaryFeed.nickname }}</p>
-          <p class="feed-content">
-            {{ selectedDiaryFeed.content }}
-          </p>
-          <p class="feed-view">👀 {{ selectedDiaryFeed.viewCount }}</p>
-          <img :src="selectedDiaryFeed.imageUrl" alt="Post Image" />
-        </div>
-      </RouterLink>
-    </div>
+      </swiper-slide>
+      <swiper-slide>
+        <CountDougnutStat />
+      </swiper-slide>
+    </swiper>
   </main>
 </template>
 
@@ -120,6 +139,27 @@ main {
   background-color: #242424;
   min-height: 100%;
   @apply flex flex-col items-center m-4;
+}
+
+.swiper {
+  width: 100%;
+}
+
+/* .swiper-slide {
+  text-align: center;
+  font-size: 18px;
+  background: #fff;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+} */
+
+.swiper-slide img {
+  display: block;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 }
 
 .loading {
@@ -184,5 +224,9 @@ main {
 }
 .feed-view {
   @apply flex flex-row;
+}
+
+.no-diary-message {
+  @apply text-white text-center m-4;
 }
 </style>
